22105e3ec6ccc77ad79b94f64e43b80c0bbfc439
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-10-01 23:34:26 -0300

    refactor: convert component to Class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import TaskItem from "./components/TaskItem";
+
+// const App = () => {
+//     const [tasks, setTasks] = useState([
+//         {
+//             id: "1",
+//             description: "Estudar Programação",
+//             isCompleted: false,
+//         },
+//         {
+//             id: "2",
+//             description: "Ler",
+//             isCompleted: true,
+//         },
+//     ]);
+//     return (
+//         <>
+//             {tasks.map((task) => (
+//                 <TaskItem key={task.id} task={task} />
+//             ))}
+//         </>
+//     );
+// };
+
+// export default App;
+
+import React from "react";
 import TaskItem from "./components/TaskItem";
 
-const App = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: "1",
-            description: "Estudar Programação",
-            isCompleted: false,
-        },
-        {
-            id: "2",
-            description: "Ler",
-            isCompleted: true,
-        },
-    ]);
-    return (
-        <>
-            {tasks.map((task) => (
-                <TaskItem key={task.id} task={task} />
-            ))}
-        </>
-    );
-};
-
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleStateChange = this.handleStateChange.bind(this);
+        this.state = {
+            tasks: [
+                {
+                    id: "1",
+                    description: "Correr",
+                    isCompleted: false,
+                },
+                {
+                    id: "2",
+                    description: "Nadar",
+                    isCompleted: true,
+                },
+            ],
+        };
+    }
+    handleStateChange() {
+        this.setState({
+            tasks: [],
+        });
+    }
+    render() {
+        return (
+            <>
+                {this.state.tasks.map((task) => (
+                    <TaskItem key={task.id} task={task} />
+                ))}
+                <button onClick={this.handleStateChange}>Limpar Tarefas</button>
+            </>
+        );
+    }
+}
 export default App;
