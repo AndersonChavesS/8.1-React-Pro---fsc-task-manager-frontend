a09327e5b1e1227949fd9294594c4ff3c6e3fd00
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-10-29 21:22:09 -0300

    feat: add AddTask component

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+import CustomInput from "./CustomInput";
+import "./AddTask.scss";
+
+const AddTask = () => {
+    const [task, setTask] = useState("");
+
+    const onChange = (e) => {
+        setTask(e.target.valeu);
+    };
+
+    return (
+        <div className="add-task-container">
+            <CustomInput
+                label="Adicionar tarefa..."
+                value={task}
+                onChange={onChange}
+            />
+        </div>
+    );
+};
+
+export default AddTask;
