e7e8d6b40d6d24eed9126d54b9257d0298d79f10
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-10-29 22:06:18 -0300

    feat: add CustomButton component

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,11 @@
+import "./CustomButton.scss";
+
+const CustomButton = ({ onClick, children }) => {
+    return (
+        <div className="custom-button-container" onClick={onClick}>
+            {children}
+        </div>
+    );
+};
+
+export default CustomButton;
