1569f0f982e4b053c692501d839584ab88f926ee
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-11-02 16:03:39 -0300

    feat: add login route

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 
 import "./App.scss";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const App = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     );
 };
