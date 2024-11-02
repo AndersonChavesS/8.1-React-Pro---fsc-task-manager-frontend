5ada404dd609a97175899c3a3e6aaa85503aadde
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-11-02 15:53:47 -0300

    feat: add Login component

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import CustomButtom from '../components/CustomButton'
+
+import "./Login.scss";
+import { logo } from "../assets/images/logo-2.png";
+
+const Login = () => {
+    return (
+        <div className="login-container">
+            {" "}
+            <img src={logo} alt="Full Stack Club" />
+            <div className="button-container">
+              <CustomButtom>Entrar</CustomButtom>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
